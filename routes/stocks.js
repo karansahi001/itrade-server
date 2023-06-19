6ef4d6e9d30bca8944a7c2a2e2fdf50b70761c51
karansahi001 <iamkaransahi@gmail.com>
2023-06-19 13:32:34 -0400

    all major routes created

--- routes/stocks.js
+++ routes/stocks.js
@@ -17,4 +17,56 @@ router.get("/stocks", (_req, res) => {
     fetchData();
 })
 
+router.get("/stocks/:ticker", (req, res) => {
+    const { ticker } = req.params
+    const fetchTickerData = async () => {
+        try{
+            const response = await axios.get(`https://api.polygon.io/v3/reference/tickers/${ticker}?apiKey=${apiKey}`)
+            res.status(200).json(response.data)
+        }catch(err){
+            res.status(400).send(err);
+        }
+    }
+    fetchTickerData();
+})
+
+router.get("/news/:ticker", (req, res) => {
+    const { ticker } = req.params
+    const fetchNewsData = async () => {
+        try{
+            const response = await axios.get(`https://api.polygon.io/v2/reference/news?ticker=${ticker}&order=asc&limit=6&sort=published_utc&apiKey=${apiKey}`)
+            res.status(200).json(response.data)
+        }catch(err){
+            res.status(400).send(err);
+        }
+    }
+    fetchNewsData();
+})
+
+router.get("/losers", (_req, res) => {
+
+    const fetchLosersData = async () => {
+        try{
+            const response = await axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/losers?apiKey=${apiKey}`)
+            res.status(200).json(response.data)
+        }catch(err){
+            res.status(400).send(err);
+        }
+    }
+    fetchLosersData();
+})
+
+router.get("/gainers", (_req, res) => {
+
+    const fetchGainersData = async () => {
+        try{
+            const response = await axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${apiKey}`)
+            res.status(200).json(response.data)
+        }catch(err){
+            res.status(400).send(err);
+        }
+    }
+    fetchGainersData();
+})
+
 module.exports = router;
